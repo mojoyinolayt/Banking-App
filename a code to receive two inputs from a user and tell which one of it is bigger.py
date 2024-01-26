70c2760fbd40e9d05f555609d35d7ca056320ecb
mojoyinolayt <153513441+mojoyinolayt@users.noreply.github.com>
2024-01-26 13:49:58 +0100

    Add files via upload

---
+++ a code to receive two inputs from a user and tell which one of it is bigger.py
@@ -0,0 +1,8 @@
+user_1 = (input(str("number:")))
+user_2 = (input(str("number:") ))
+def check_if_num_is_large(user_1, user_2):
+    if user_1 > user_2:
+        print('it is greater than')
+    else:
+        print("null")
+check_if_num_is_large(user_1, user_2)
